fc9eadd7baca1e2eb9c895eb08ec6b21fece1973
Jakub Trzebiatowski <jtrzebiatowski@atlassian.com>
2021-04-15 17:56:36 +0200

    improvements

--- main.py
+++ main.py
@@ -20,6 +20,10 @@ def right_hand(inventory):
     return inventory + pyautogui.Point(180, 185)
 
 
+def left_hand(inventory):
+    return inventory + pyautogui.Point(20, 185)
+
+
 def arrow_slot(inventory):
     return inventory + pyautogui.Point(180, 235)
 
@@ -38,7 +42,9 @@ def wait_for_mana():
     while not mana:
         print('waiting for mana')
         mana = pyautogui.locateOnScreen('mana.png')
-        pyautogui.sleep(10)
+        if not mana:
+            pyautogui.sleep(5)
+    return center(mana)
 
 
 def move_up_bp():
@@ -49,26 +55,33 @@ def move_up_bp():
 if __name__ == '__main__':
 
     while True:
-        wait_for_mana()
-        pyautogui.sleep(random.randint(1, 20))
-        blank_rune = find_blank_rune()
-        if not blank_rune:
-            print('blank rune not found, moving up')
-            move_up_bp()
-            pyautogui.sleep(random.randint(1, 3))
+        try:
+            mana = wait_for_mana()
+            # click on mana to focus the client
+            pyautogui.click(mana)
+
+            pyautogui.sleep(random.randint(1, 5))
             blank_rune = find_blank_rune()
+            if not blank_rune:
+                print('blank rune not found, moving up')
+                move_up_bp()
+                pyautogui.sleep(random.randint(1, 3))
+                blank_rune = find_blank_rune()
 
-        if not blank_rune:
-            print('blank rune not found after moving bp up. halting')
-        pyautogui.moveTo(blank_rune)
+            if not blank_rune:
+                print('blank rune not found after moving bp up. halting')
+            pyautogui.moveTo(blank_rune)
 
-        inventory = find_inventory()
-        pyautogui.dragTo(right_hand(inventory), button='left')
-        pyautogui.sleep(random.randint(1, 10))
-        pyautogui.press('f6')
+            inventory = find_inventory()
+            pyautogui.dragTo(right_hand(inventory), button='left')
+            pyautogui.sleep(random.randint(1, 5))
+            pyautogui.press('f6')
 
-        # eat food from arrow slot
-        pyautogui.sleep(random.randint(1, 10))
-        inventory = find_inventory()
-        pyautogui.click(arrow_slot(inventory), button='right')
-
+            # eat food from arrow slot and left hand
+            pyautogui.sleep(random.randint(1, 5))
+            inventory = find_inventory()
+            pyautogui.click(arrow_slot(inventory), button='right')
+            pyautogui.sleep(random.randint(1, 5))
+            pyautogui.click(left_hand(inventory), button='right')
+        except Exception as e:
+            print(e)
